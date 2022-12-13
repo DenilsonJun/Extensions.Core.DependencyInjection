2842d5c73ab92fe6402ec800f34486028cd8548a
Denilson da Silva Júnior <denilson.junior@globalsys.com.br>
2022-12-13 20:50:36 -0300

    Add project files.

---
+++ Extensions.Core.DependencyInjection/MediatRExtensions.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Core.DependencyInjection;
+
+public static class MediatRExtensions
+{
+    public static IServiceCollection AddMediatR<T>(this IServiceCollection services)
+        => services.AddMediatR(typeof(T));
+}
