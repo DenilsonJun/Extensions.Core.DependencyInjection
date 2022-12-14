2a603a29e731462249607307941ab0235613a3c5
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-12-13 22:05:14 -0300

    #1 -  AddFluentValidation

---
+++ Extensions.Core.DependencyInjection/FluentValidationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Extensions.Core.DependencyInjection;
+
+public static class FluentValidationExtensions
+{
+    public static IServiceCollection AddFluentValidation(this IServiceCollection services, params Assembly[] assemblies)
+
+        => services.AddValidatorsFromAssemblies(assemblies);
+
+    public static IServiceCollection AddFluentValidation<T>(this IServiceCollection services)
+        => services.AddFluentValidation(typeof(T).Assembly);
+
+    public static IServiceCollection AddFluentValidation<T, T1>(this IServiceCollection service)
+        => service.AddFluentValidation(typeof(T).Assembly, typeof(T1).Assembly);
+
+    public static IServiceCollection AddFluentValidation<T, T1, T2>(this IServiceCollection service)
+        => service.AddFluentValidation(typeof(T).Assembly, typeof(T1).Assembly, typeof(T2).Assembly);
+}
