f89aa3f12e2d741243dbd79a5ca1ee9c9d45674c
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-12-13 21:01:08 -0300

    #1 - Sobrecarga de método AddMediatR

--- Extensions.Core.DependencyInjection/MediatRExtensions.cs
+++ Extensions.Core.DependencyInjection/MediatRExtensions.cs
@@ -7,4 +7,10 @@ public static class MediatRExtensions
 {
     public static IServiceCollection AddMediatR<T>(this IServiceCollection services)
         => services.AddMediatR(typeof(T));
+
+    public static IServiceCollection AddMediatR<T, T1>(this IServiceCollection services)
+        => services.AddMediatR(typeof(T), typeof(T1));
+
+    public static IServiceCollection AddMediatR<T, T1, T2>(this IServiceCollection services)
+        => services.AddMediatR(typeof(T), typeof(T1), typeof(T2));
 }
