4c79c1e8a00697f8e70a30232714900faa8d2a1a
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-12-13 21:23:34 -0300

    #1 - Adicionado AutoMapper

---
+++ Extensions.Core.DependencyInjection/AutoMapperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Core.DependencyInjection;
+
+public static class AutoMapperExtensions
+{
+    public static IServiceCollection AddAutoMapper<T>(this IServiceCollection services) where T : Profile
+        => services.AddAutoMapper(typeof(T).Assembly);
+
+    public static IServiceCollection AddAutoMapper<T, T1>(this IServiceCollection services) where T : Profile where T1 : Profile
+        => services.AddAutoMapper(typeof(T).Assembly, typeof(T1).Assembly);
+
+    public static IServiceCollection AddAutoMapper<T, T1, T2>(this IServiceCollection services) where T : Profile where T1 : Profile where T2 : Profile
+        => services.AddAutoMapper(typeof(T).Assembly, typeof(T1).Assembly, typeof(T2).Assembly);
+}
