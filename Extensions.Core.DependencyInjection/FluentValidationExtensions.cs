bc2755d4fd2faf82489faf05200bf4665f93e1e1
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-12-13 22:06:04 -0300

    #1 - AddFluentValidation

--- Extensions.Core.DependencyInjection/FluentValidationExtensions.cs
+++ Extensions.Core.DependencyInjection/FluentValidationExtensions.cs
@@ -7,15 +7,14 @@ namespace Extensions.Core.DependencyInjection;
 public static class FluentValidationExtensions
 {
     public static IServiceCollection AddFluentValidation(this IServiceCollection services, params Assembly[] assemblies)
-
         => services.AddValidatorsFromAssemblies(assemblies);
 
     public static IServiceCollection AddFluentValidation<T>(this IServiceCollection services)
         => services.AddFluentValidation(typeof(T).Assembly);
 
-    public static IServiceCollection AddFluentValidation<T, T1>(this IServiceCollection service)
-        => service.AddFluentValidation(typeof(T).Assembly, typeof(T1).Assembly);
+    public static IServiceCollection AddFluentValidation<T, T1>(this IServiceCollection services)
+        => services.AddFluentValidation(typeof(T).Assembly, typeof(T1).Assembly);
 
-    public static IServiceCollection AddFluentValidation<T, T1, T2>(this IServiceCollection service)
-        => service.AddFluentValidation(typeof(T).Assembly, typeof(T1).Assembly, typeof(T2).Assembly);
+    public static IServiceCollection AddFluentValidation<T, T1, T2>(this IServiceCollection services)
+        => services.AddFluentValidation(typeof(T).Assembly, typeof(T1).Assembly, typeof(T2).Assembly);
 }
